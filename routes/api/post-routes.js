313c694e5aa230976d3b1a4c04122feb0b7a6320
Apo11o <jeffknoespel@gmail.com>
2021-09-26 18:59:38 -0500

    refactoring

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
 const { Post, User } = require('../../models');
+const sequelize = require('../../config/connection');
 
 // get all users
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 
+        'post_url', 
+        'title', 
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_cout']
+    ],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -27,11 +33,23 @@ router.get('/:id', (req, res) => {
       where: {
         id: req.params.id
       },
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
       include: [
         {
-          model: User,
-          attributes: ['username']
+          model: Post,
+          attributes: ['id', 'title', 'post_url', 'created_at']
+        },
+        {
+          model: Post,
+          attributes: ['title'],
+          through: Vote,
+          as: 'voted_posts'
         }
       ]
     })
@@ -60,6 +78,17 @@ router.post('/', (req, res) => {
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
+      });
+});
+
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+      .then(updatedPostData => res.json(updatedPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
       });
 });
 
